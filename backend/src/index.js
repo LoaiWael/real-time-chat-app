8c2fc85fdf62f3d5f078ef78763b1d90aa969798
Loai Wael <loaiwael.dev@gmail.com>
2026-07-24 15:22:46 +0300

    UPD backend server setup with mongoose connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,16 @@
 import express from "express";
+import mongoose from 'mongoose';
 import "dotenv/config";
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log('Database connected');
+}).catch((error) => {
+  console.log(error);
+})
