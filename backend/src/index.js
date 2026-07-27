1e9ab831c924cb2745ca26d61c4c3fbc4e459d89
Loai Wael <loaiwael.dev@gmail.com>
2026-07-27 23:07:49 +0300

    feat: implement multi-stage Dockerfile and static file serving in backend for production deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,10 +3,14 @@ import "dotenv/config";
 import connectDB from "./lib/db.js";
 import { clerkMiddleware } from '@clerk/express'
 import cors from 'cors'
+import fs from "fs";
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const FRONTEND_URL = process.env.FRONTEND_URL;
+
+const publicDir = path.join(process.cwd(), "public");
 
 app.use(express.json());
 app.use(cors({ origin: FRONTEND_URL, credentials: true }));
@@ -15,6 +19,16 @@ app.use(clerkMiddleware());
 app.get("/health", (_, res) => {
   res.status(200).json({ ok: true })
 })
+
+// if the public directory exists, serve the static files
+// this is for the production build
+if (fs.existsSync(publicDir)) {
+  app.use(express.static(publicDir));
+
+  app.get("/{*any}", (req, res, next) => {
+    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+  });
+}
 
 app.listen(PORT, async () => {
   console.log(`Server started on port ${PORT}`);
